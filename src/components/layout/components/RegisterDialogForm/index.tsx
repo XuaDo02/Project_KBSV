1f4fb31d99d1e95a338d56b44178b90b88f4105c
XuaDo02 <dothixua20020312@gmail.com>
2024-02-26 09:27:07 +0700

    commit_update_register

--- src/components/layout/components/RegisterDialogForm/index.tsx
+++ src/components/layout/components/RegisterDialogForm/index.tsx
@@ -1,99 +1,93 @@
-import { useEffect, useState } from "react";
+import React from "react";
 import { TableData } from "src/types/tabledata";
 
 interface DialogFormProps {
-    selectedItem: TableData;
-    sendStatusDialog: (val: boolean) => void;
+  sendStatusDialog: (val: boolean) => void;
+  selectedItem: TableData;
 }
 
-const DialogForm: React.FC<DialogFormProps> = ({ selectedItem, sendStatusDialog }) => {
+const DialogForm: React.FC<DialogFormProps> = ({
+  selectedItem,
+  sendStatusDialog
+}) => {
+  const inputFields = [
+    { label: "Tiểu khoản", className: "bg-customDark3 py-1" },
+    { label: "Mã", className: "bg-customDark3 py-1", value: selectedItem.id },
+    {
+      label: "Giá",
+      className: "bg-customDark3 py-1",
+      value: selectedItem.price
+    },
+    {
+      label: "SL CK hưởng quyền",
+      className: "bg-customDark3 py-1",
+      value: selectedItem.slckhq
+    },
+    {
+      label: "SL quyền sở hữu",
+      className: "bg-customDark3 py-1",
+      value: selectedItem.slqsh
+    },
+    {
+      label: "SL CK đã mua",
+      className: "bg-customDark3 py-1",
+      value: selectedItem.slckdm
+    },
+    {
+      label: "SL CK còn được mua",
+      className: "bg-customDark3 py-1",
+      value: selectedItem.slckcdm
+    },
+    {
+      label: "SL CK đăng ký mua",
+      className:
+        "border border-customGray rounded text-right py-1 flex bg-customDark3 text-customDark3",
+      placeholder: "Nhập số lượng"
+    },
+    { label: "Tiền mua phải thanh toán", className: "bg-customDark3 py-1" }
+  ];
 
-    const handleCancelClick = () => {
-        sendStatusDialog(false)
-    }
-    return (
-        <div className="fixed inset-0 flex items-center justify-center">
-            <div className="bg-neutral-800 p-6 rounded shadow-md w-1/2 h-auto">
-                <div>
-                    <h2 className="text-lg font-semibold text-white mb-4">Đăng ký quyền mua</h2>
-                </div>
-                <div className="mb-2 flex items-center">
-                    <label className="block text-sm font-medium text-zinc-400 w-1/2 text-left pr-4">
-                        Tiểu khoản
-                    </label>
-                    <input className="bg-neutral-800 py-1 text-white" />
-                </div>
+  const handleCancelClick = () => {
+    sendStatusDialog(false);
+  };
 
-                <div className="mb-2 flex items-center">
-                    <label className="block text-sm font-medium text-zinc-400 w-1/2 text-left pr-4">
-                        Mã 
-                    </label>
-                    <input className="bg-neutral-800 py-1 text-white" value={selectedItem.id}/>
-                </div>
-                
-                <div className="mb-2 flex items-center">
-                    <label className="block text-sm font-medium text-zinc-400 w-1/2 text-left pr-4">
-                        SL CK hưởng quyền
-                    </label>
-                    <input className="bg-neutral-800 py-1 text-white" value={selectedItem.slckhq}/>
-                </div>
-                <div className="mb-2 flex items-center">
-                    <label className="block text-sm font-medium text-zinc-400 w-1/2 text-left pr-4">
-                        SL quyền sở hữu
-                    </label>
-                    <input className="bg-neutral-800 py-1 text-white" value={selectedItem.slqsh}/>
-                </div>
-                <div className="mb-2 flex items-center">
-                    <label className="block text-sm font-medium text-zinc-400 w-1/2 text-left pr-4">
-                        Giá
-                    </label>
-                    <input className="bg-neutral-800 py-1 text-white" value={selectedItem.price}/>
-                </div>
-                <div className="mb-2 flex items-center">
-                    <label className="block text-sm font-medium text-zinc-400 w-1/2 text-left pr-4">
-                        SL CK đã mua
-                    </label>
-                    <input className="bg-neutral-800 py-1 text-white" value={selectedItem.slckdm}/>
-                </div>
-                <div className="mb-2 flex items-center">
-                    <label className="block text-sm font-medium text-zinc-400 w-1/2 text-left pr-4">
-                        SL CK còn được mua
-                    </label>
-                    <input className="bg-neutral-800 py-1 text-white" value={selectedItem.slckcdm}/>
-                </div>
-                <div className="mb-2 flex items-center">
-                    <label className="block text-sm font-medium text-zinc-400 w-1/2 text-left pr-4">
-                        SL CK đăng ký mua
-                    </label>
-                    <input type="text" className="border border-neutral-500 rounded text-right py-1 flex" placeholder="Nhập số lượng" />
-                </div>
-                <div className="mb-2 flex items-center">
-                    <label className="block text-sm font-medium text-zinc-400 w-1/2 text-left pr-4">
-                        Tiền mua phải thanh toán
-                    </label>
-                    <input className="bg-neutral-800 py-1"/>
-                </div>
-
-                <div className="mt-5">
-                    <hr className="border-t border-neutral-600 w-full" />
-                </div>
-                
-                <div className="flex justify-center mt-3 ">
-                    <button
-                        onClick={handleCancelClick}
-                        className="mr-2 px-4 py-2 text-sm font-medium text-yellow-500 bg-neutral-800 border border-orange-500 rounded-md"
-                    >
-                        Huỷ
-                    </button>
-                    <button
-                        className="px-4 py-2 text-sm font-medium text-white bg-neutral-600 rounded-md "
-                    >
-                        Đăng ký
-                    </button>
-                </div>
-            </div>
+  return (
+    <div className="fixed inset-0 flex items-center justify-center">
+      <div className="bg-customDark3 p-6 rounded shadow-md w-1/2 h-auto">
+        <div>
+          <h2 className="text-lg font-semibold text-white mb-4 bg-customBlack text-left">
+            Đăng ký quyền mua
+          </h2>
         </div>
-    );
+        {inputFields.map((inputField, index) => (
+          <div key={index} className="mb-2 flex items-center  ">
+            <label className="block text-sm font-medium text-zinc-400 w-1/2 text-left pr-4  ">
+              {inputField.label}
+            </label>
+            <input
+              className={inputField.className}
+              placeholder={inputField.placeholder}
+              value={inputField.value}
+            />
+          </div>
+        ))}
+        <div className="mt-5">
+          <hr className="border-t border-neutral-600 w-full" />
+        </div>
+        <div className="flex justify-center mt-3">
+          <button
+            onClick={handleCancelClick}
+            className="mr-2 px-4 py-2 text-sm font-normal text-customYellow bg-neutral-800 border border-customYellow rounded-md"
+          >
+            Huỷ
+          </button>
+          <button className="px-4 py-2 text-sm font-normal text-customWhite bg-customGray rounded-md">
+            Đăng ký
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default DialogForm;
--- src/components/layout/components/RegisterLayout/RegisterTable/RegisterTableData.tsx
+++ src/components/layout/components/RegisterLayout/RegisterTable/RegisterTableData.tsx
@@ -1,56 +1,66 @@
-
 import { useState } from "react";
 import { TableData } from "src/types/tabledata";
 import DialogForm from "../../RegisterDialogForm";
 
 interface RegisterTableDataProps {
-    tableData: TableData[];
+  tableData: TableData[];
 }
 
 const RegisterTableData: React.FC<RegisterTableDataProps> = ({ tableData }) => {
-    const [selectedItem, setSelectedItem] = useState<TableData | null>(null);
-    const [showDialog, setShowDialog] = useState(false);
+  const [selectedItem, setSelectedItem] = useState<TableData | null>(null);
+  const [showDialog, setShowDialog] = useState(false);
 
-    const handleRegisterClick = (item: TableData) => {
-        setSelectedItem(item);
-        setShowDialog(true);
-    };
-    const getStatusDialogRegister = (val: boolean) => {
-        setShowDialog(val);
-    }
+  const handleRegisterClick = (item: TableData) => {
+    setSelectedItem(item);
+    setShowDialog(true);
+  };
+  const getStatusDialogRegister = (val: boolean) => {
+    setShowDialog(val);
+  };
 
-    return (
-        <>
-            {tableData.map((item, index) => (
-                <div className={`flex`}>
-                    <div className={`grid grid-cols-12 text-white text-center items-center w-full text-xs  ${index % 2 == 0 ? `bg-neutral-800` : `bg-neutral-900`}`}>
-                        <div className="grid col-span-4 grid-cols-8 py-2">
-                            <div className="col-span-1">{item.id}</div>
-                            <div className="col-span-1">{item.symbol}</div>
-                            <div className="col-span-3">{item.slckhq}</div>
-                            <div className="col-span-3">{item.slqsh}</div>
-                        </div>
-                        <div className="grid col-span-4 grid-cols-6">
-                            <div className="col-span-1">{item.price}</div>
-                            <div className="col-span-1">{item.term}</div>
-                            <div className="col-span-2">{item.termdk}</div>
-                            <div className="col-span-2">{item.slckcdm}</div>
-                        </div>
-                        <div className="grid col-span-4 grid-cols-5">
-                            <div className="col-span-1">{item.slckdm}</div>
-                            <div className="col-span-1">{item.paid}</div>
-                            <div className="col-span-2">{item.payable}</div>
-                            <div className="col-span-1 h-full flex justify-center items-center text-xs text-yellow-400 underline">
-                            <button onClick={() => handleRegisterClick(item)}>Đăng ký</button>
-                            </div>
-                            {showDialog && selectedItem && <DialogForm selectedItem={selectedItem} sendStatusDialog={getStatusDialogRegister} />}
-                        </div>
-                    </div>
-                </div>
-
-            ))}
-        </>
-    );
+  return (
+    <>
+      {tableData.map((item, index) => (
+        // eslint-disable-next-line react/jsx-key
+        <div className={"flex"}>
+          <div
+            className={`grid grid-cols-12 text-white text-center items-center w-full text-xs  ${
+              index % 2 === 0 ? `bg-neutral-800` : `bg-neutral-900`
+            }`}
+          >
+            <div className="grid col-span-4 grid-cols-8 py-2">
+              <div className="col-span-1">{item.id}</div>
+              <div className="col-span-1">{item.symbol}</div>
+              <div className="col-span-3">{item.slckhq}</div>
+              <div className="col-span-3">{item.slqsh}</div>
+            </div>
+            <div className="grid col-span-4 grid-cols-6">
+              <div className="col-span-1">{item.price}</div>
+              <div className="col-span-1">{item.term}</div>
+              <div className="col-span-2">{item.termdk}</div>
+              <div className="col-span-2">{item.slckcdm}</div>
+            </div>
+            <div className="grid col-span-4 grid-cols-5">
+              <div className="col-span-1">{item.slckdm}</div>
+              <div className="col-span-1">{item.paid}</div>
+              <div className="col-span-2">{item.payable}</div>
+              <div className="col-span-1 h-full flex justify-center items-center text-xs text-yellow-400 underline">
+                <button onClick={() => handleRegisterClick(item)}>
+                  Đăng ký
+                </button>
+              </div>
+              {showDialog && selectedItem && (
+                <DialogForm
+                  selectedItem={selectedItem}
+                  sendStatusDialog={getStatusDialogRegister}
+                />
+              )}
+            </div>
+          </div>
+        </div>
+      ))}
+    </>
+  );
 };
 
 export default RegisterTableData;
